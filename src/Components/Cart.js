8ff549b12bbc3985c4415833d0a287763a243188
Martin Reboredo <martinrg33@gmail.com>
2019-08-27 17:37:02 +0200

    add use effect

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -17,30 +17,31 @@ function Cart() {
     }
   ]);
 
-  useEffect(() => postRequest(wishlist), [wishlist]);
-
-   function postRequest(data) {
-    // console.log(value) // Before the Post Request return the Positive ID - the item on wishlist has a negative ID
-    // Identify the last Persistent Item in wishlist in order to assing the
-    // data.map((value) => {
-    //   console.log(value)
-    // });
-
-// SIMULATE RANDOM RESPONSE TIME FROM SERVER
-    function responseTime(max) {
-      return Math.floor(Math.random() * Math.floor(max));
+  useEffect( ()=> {
+    const fetchData = async () => {
+      const result = await function postRequest(data) {
+        // SIMULATE RANDOM RESPONSE TIME FROM SERVER
+            function responseTime(max) {
+              return Math.floor(Math.random() * Math.floor(max));
+            }
+            //UPDATE THE ID OF THE WISHLIST ITEM WITH LATENCY TO SIMULATE A SERVER CALL TO
+            setTimeout(() => {
+              let newID = data[data.length - 1].id;
+              let posID = newID > 1
+                ? newID
+                : data[data.length - 2].id + 1;
+              wishlist[wishlist.length - 1].id = posID;
+              console.log(wishlist);
+            }, responseTime(5000));  
+        
+            return wishlist
+          };
+      setWishlist(result);
     }
-    //UPDATE THE ID OF THE WISHLIST ITEM WITH LATENCY TO SIMULATE A SERVER CALL TO
-    setTimeout(() => {
-      let newID = data[data.length - 1].id;
-      let posID = newID > 1
-        ? newID
-        : data[data.length - 2].id + 1;
-      wishlist[wishlist.length - 1].id = posID;
-      console.log(wishlist);
-    }, responseTime(5000));  
+    fetchData();
+  
+  }, [wishlist]);
 
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
